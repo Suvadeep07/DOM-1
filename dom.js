8597990ed5f88a432234272b32da64c0acaa1d09
Suvadeep Mukherjee1 <mukherjee07.suvadeep@gmail.com>
2023-06-02 21:30:56 +0530

    final manipulation

---
+++ dom.js
@@ -0,0 +1,11 @@
+
+var headerTitle = document.getElementById('header-title');
+var header = document.getElementById('main-header');
+
+
+header.style.borderBottom="solid 3px #000"
+var main = document.getElementById("main");
+  var addItemTitle = main.getElementsByClassName("title")[0];
+
+  addItemTitle.style.fontWeight = "bold";
+  addItemTitle.style.color = "green";
