db9e6591bdbd5cba5064c854fd850e380d6c09be
Suvadeep07 <135342474+Suvadeep07@users.noreply.github.com>
2023-06-03 13:08:22 +0530

    made some changes
    Made the 2nd item have green background color
    Made the 3rd item invisible
    Using QuerySelectorALL changed the font color to green for 2nd item in the item list
    Choosed all the odd elements and make their background green using QuerySelectorALL﻿

--- dom.js
+++ dom.js
@@ -1,14 +1,48 @@
-var items =document.getElementsByClassName('list-group-item');
+//var items =document.getElementsByClassName('list-group-item');
 ////console.log(items);
 //items[2].textContent='Hello2';
 //items[1].style.fontWeight = 'bold';
 //items[2].style.backgroundColor ='green';
-for(var i=0;i<items.length;i++){
-  items[i].style.fontWeight='bold'
-}
+//for(var i=0;i<items.length;i++){
+  //items[i].style.fontWeight='bold'
+//}
 
-var li = document.getElementsByTagName('li');
-console.log(li);
-for(var i=0;i<li.length;i++){
-  li[i].style.backgroundColor='#f4f4f4';
+//var li = document.getElementsByTagName('li');
+//console.log(li);
+//for(var i=0;i<li.length;i++){
+  //li[i].style.backgroundColor='#f4f4f4';
+//}
+
+var header = document.querySelector('#main-header');
+header.getElementsByClassName.borderBottom = 'solid 4px #ccc';
+var input = document.querySelector('input');
+input.value='Hello World';
+var submit = document.querySelector('input[type="submit"]');
+submit.value="SEND";
+
+var item = document.querySelector('.list-group-item');
+item.style.color='red';
+
+var lastItem=document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem =document.querySelector('.list-group-item:nth-child(2)');
+//secondItem.style.color= ' green';
+
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'hello';
+
+var items = document.querySelectorAll('.list-group-item');
+items[2].style.display = 'none';
+
+var odd =document.querySelectorAll('li:nth-child(odd');
+for(var i=0;i<odd.length;i++){
+  odd[i].style.backgroundColor= '#f4f4f4';
 }
+var items = document.querySelectorAll('.list-group-item');
+items[1].style.color = 'green';
+
+
+
+
