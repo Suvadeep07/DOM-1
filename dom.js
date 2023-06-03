d810ea1c076fc098743416b802e5541b62d74b5e
Suvadeep07 <135342474+Suvadeep07@users.noreply.github.com>
2023-06-03 12:31:57 +0530

    tried editing by tag-name , class name

--- dom.js
+++ dom.js
@@ -1,9 +1,14 @@
 var items =document.getElementsByClassName('list-group-item');
-console.log(items);
+////console.log(items);
 //items[2].textContent='Hello2';
 //items[1].style.fontWeight = 'bold';
-items[2].style.backgroundColor ='green';
+//items[2].style.backgroundColor ='green';
 for(var i=0;i<items.length;i++){
   items[i].style.fontWeight='bold'
 }
-//heloo111
+
+var li = document.getElementsByTagName('li');
+console.log(li);
+for(var i=0;i<li.length;i++){
+  li[i].style.backgroundColor='#f4f4f4';
+}
